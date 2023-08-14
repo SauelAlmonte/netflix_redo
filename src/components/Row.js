4bc314bf5bab05856e7395a0bb33552ab53dfebc
Hack <almontesauel@gmail.com>
2023-08-14 15:15:09 -0400

    Created Movie component, inherited item function, and added styling

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,10 +1,9 @@
 import axios from "axios";
-import { FaHeart, FaRegHeart } from "react-icons/fa";
 import React, { useEffect, useState } from "react";
+import { Movie } from "./Movie";
 
 const Row = ({ title, fetchURL }) => {
   const [movies, setMovies] = useState([]);
-  const [like, setLike] = useState(false);
 
   useEffect(() => {
     axios.get(fetchURL).then((response) => {
@@ -19,26 +18,7 @@ const Row = ({ title, fetchURL }) => {
       <div className="relative flex items-center">
         <div id={"slider"}>
           {movies.map((item, id) => (
-            <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
-              <img
-                className="w-full h-auto block"
-                src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
-                alt={item?.title}
-              />
-
-              <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
-                <p className="whitespace-normal text-sm md:text-base font-bold flex justify-center items-center h-full text-center">
-                  {item?.title}
-                </p>
-                <p>
-                  {like ? (
-                    <FaHeart className="absolute top-4 left-4 text-gray-300" />
-                  ) : (
-                    <FaRegHeart className="absolute top-4 left-4 text-gray-300" />
-                  )}
-                </p>
-              </div>
-            </div>
+            <Movie key={id} item={item}/>
           ))}
         </div>
       </div>
