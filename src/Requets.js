9bc2edc36d2eb8af360a68fb35316ac921f7035c
Hack <almontesauel@gmail.com>
2023-08-14 13:42:25 -0400

    Created Home Page, added API, updated Mian page with image, and context

---
+++ src/Requets.js
@@ -0,0 +1,11 @@
+const key = "f5beff784f82209a9e3bb0bf79c204d1";
+
+const requests = {
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+  requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests;
