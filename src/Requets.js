9395199532ace288c226034f626a03bfbbcdd2f8
Hack <almontesauel@gmail.com>
2023-08-14 15:09:10 -0400

    Created Row component, implemented apy get, and styling

--- src/Requets.js
+++ src/Requets.js
@@ -1,6 +1,6 @@
 const key = "f5beff784f82209a9e3bb0bf79c204d1";
 
-const requests = {
+export const requests = {
   requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
   requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
   requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
