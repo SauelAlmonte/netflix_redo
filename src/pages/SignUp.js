119a02810ac4aa3476d2e4915799951346075fbe
Hack <almontesauel@gmail.com>
2023-08-14 23:02:16 -0400

    Implemented LogIn and SignUp styling

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
+    <>
+      <div className="w-full h-screen">
+        <img
+          className="hidden sm:block absolute w-full h-full object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/6c884f48-f7d8-4a59-9d25-b7c138813aee/c741e848-5f1b-4230-8400-909aa0a4ac80/US-en-20230807-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="/"
+        />
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-screen">
+          <div className="fixed w-full px-4 py-24 z-50">
+            <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
+              <div className="max-w-[320px] mx-auto py-16">
+                <h1 className="text-3xl font-bold">Sign Up</h1>
+                <form className="w-full flex flex-col py-4">
+                  <input
+                    className="p-3 my-2 bg-gray-700 rounded "
+                    type="email"
+                    placeholder="Email"
+                    autoComplete="email"
+                  />
+                  <input
+                    className="p-3 my-2 bg-gray-700 rounded "
+                    type="password"
+                    placeholder="Password"
+                    autoComplete="current-password"
+                  />
+                  <button className="bg-red-600 py-3 my-6 rounded font-bold">
+                    Sign Up
+                  </button>
+                  <div className="flex justify-between items-center text-sm text-gray-600">
+                    <p>
+                      <input className="mr-2" type="checkbox" />
+                      Remeber Me
+                    </p>
+                    <p>Need Help?</p>
+                  </div>
+                  <p className="py-8">
+                    <span className="text-gray-600 mr-2">
+                      Already Subscribed to Netflix?
+                    </span>
+                    {""}
+                    <Link to="/login" className="underline hover:font-bold">Sign In</Link>
+                  </p>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default SignUp
+export default SignUp;
