e807e9814ca2b8f6b8add09f04a54dd452030883
Hack <almontesauel@gmail.com>
2023-08-14 21:11:39 -0400

    Implemented Scroll Left and Scroll Right interactivity

--- src/components/Row.js
+++ src/components/Row.js
@@ -11,13 +11,23 @@ const Row = ({ title, fetchURL }) => {
       setMovies(response.data.results);
     });
   }, [fetchURL]);
-  console.log(movies);
+
+  const slideLeft = () => {
+    let slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+
+  const slideRight = () => {
+    let slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
 
   return (
     <>
       <h2 className="text-white font-bold :text-xl p-4">{title}</h2>
       <div className="relative flex items-center group">
         <MdChevronLeft
+          onClick={slideLeft}
           className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
           size={40}
         />
@@ -30,6 +40,7 @@ const Row = ({ title, fetchURL }) => {
           ))}
         </div>
         <MdChevronRight
+          onClick={slideRight}
           className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
           size={40}
         />
