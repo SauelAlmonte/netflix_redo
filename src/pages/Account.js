9b1049e4490b4a55fbfd7c44eb52bb99fe79cbf5
Hack <almontesauel@gmail.com>
2023-08-15 23:42:06 -0400

    Implemented logic for Account saved show view, applied styling

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Account = () => {
   return (
-    <div>Account</div>
-  )
-}
+    <>
+      <div className="w-full text-white">
+        <img
+          className="w-full h-[400px] object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/6c884f48-f7d8-4a59-9d25-b7c138813aee/c741e848-5f1b-4230-8400-909aa0a4ac80/US-en-20230807-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="/"
+        />
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-[550px]">
+          <div className="absolute top-[20%] p-4 md:p-8">
+            <h1 className="text-3xl md:text-5xl font-bold">My Shows</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Account
+export default Account;
